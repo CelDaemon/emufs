f4e27f61c4aa44c231630bb86f785bae72cfb81b
CelDaemon <celdaemon@voidgroup.net>
2025-12-19 16:52:27 +0100

    Allow snapshots script to work with header instead of subject line

--- snapshots.sh
+++ snapshots.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -ue
 script_dir="$(dirname "$0")"
 replace_bin="$script_dir/replace.py"
@@ -7,16 +7,13 @@ snapshots_dir="$(mktemp -d)"
 work_dir="$(mktemp -d)"
 trap "rm -rf '$snapshots_dir' '$work_dir'" EXIT
 
-IFS=$'\n' commit_lines=($(git log --grep='Level: .\+' --format='%H%x09%f' emudevz))
-
 git worktree add --detach "$work_dir" emudevz
 "$replace_bin" ./save.devz ./save.devz "$work_dir/code" /code
 git worktree remove "$work_dir"
 
-for commit in "${commit_lines[@]}"
+git log --grep='Level: .\+' --format='%H' emudevz | while read -r commit_hash
 do
-    commit_hash="$(printf "$commit" | cut -f1)"
-    snapshot_name="$(printf "$commit" | cut -f2 | tr '[:upper:]' '[:lower:]')"
+    snapshot_name="level-$(git show --format='%b' --no-patch "$commit_hash" | sed -nE 's/.*Level: ([a-z0-9\-]+).*/\1/p')"
     git worktree add --detach "$work_dir" "$commit_hash"
     cp -rT "$work_dir/code" "$snapshots_dir/$snapshot_name"
     git worktree remove "$work_dir"
